41858c9db9753f6b68c397d0a2daaaf3f3fc6bc3
Inthrayuth Mahaphol and Lilo Onwuzu <inthrayuth+lilo@>
2016-04-11 14:22:55 -0700

    Added module to set and reset fields. Show adoption status

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,77 @@
-functions
-prototypes
+// functions
+// prototypes
+function set(){
+var inputName = $("input#name").val();
+var inputAddress = $("input#address").val();
+var inputPhone = $("input#phone").val();
+var inputEmail = $("input#email").val();
+}
+function reset(){
+$("input#name").val("");
+$("input#address").val("");
+$("input#phone").val("");
+$("input#email").val("");
+}
 
 $(document).ready(function(){
-  $("button .adopt").click(function(){
-    $("#form-adopt").toggle();
+  $(".dog1").click(function(){
+    $("#form-adopt").show();
+    $("#form-adopt").submit(function(event){
+      event.preventDefault();
+      set();
+      $("#output").show();
+      $(".new-dog1").show();
+      $(".dog1").hide();
+      reset();
+    });
   });
 
-  $("#form-adopt").submit(function(event){
-    event.preventDefault();
+  $(".dog2").click(function(){
+    $("#form-adopt").show();
+    $("#form-adopt").submit(function(event){
+      event.preventDefault();
+      set();
+      $("#output").show();
+      $(".new-dog2").show();
+      $(".dog2").hide();
+      reset();
+    });
+  });
 
-    var inputName = $("input#name").val();
-    var inputAddress = $("input#address").val();
-    var inputPhone = $("input#phone").val();
-    var inputEmail = $("input#email").val();
+  $(".cat1").click(function(){
+    $("#form-adopt").show();
+    $("#form-adopt").submit(function(event){
+      event.preventDefault();
+      set();
+      $("#output").show();
+      $(".new-cat1").show();
+      $(".cat1").hide();
+      reset();
+    });
+  });
 
-    $("#output").show();
-    $(".danger").show();
+  $(".cat2").click(function(){
+    $("#form-adopt").show();
+    $("#form-adopt").submit(function(event){
+      event.preventDefault();
+      set();
+      $("#output").show();
+      $(".new-cat2").show();
+      $(".cat2").hide();
+      reset();
+    });
+  });
+
+  $(".animal").click(function(){
+    $("#form-adopt").show();
+    $("#form-adopt").submit(function(event){
+      event.preventDefault();
+      set();
+      $("#output").show();
+      $(".new-animal").show();
+      $(".animal").hide();
+      reset();
+    });
   });
 
 });
